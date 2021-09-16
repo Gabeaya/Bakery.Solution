9758a5fe79fd1faad092a841e222466fdcf3003b
Gabriel Ayala <gabeayala100@gmail.com>
2021-09-16 11:47:42 -0700

    Add constructor for the amount set.

--- Bakery/Models/Goods.cs
+++ Bakery/Models/Goods.cs
@@ -22,6 +22,17 @@ namespace Bakery.Models
       Price = Amount * 5;
       return Price;
     }
-    
+  }
+
+  public class Pastry
+  {
+    public int Amount { get; set; }
+    public int Price { get; set; }
+
+    public Pastry (int amount, int price=0)
+    {
+      Amount = amount;
+      Price = price;
+    }
   }
 }
