d7fd884f85399439283160e549dca2ecfa376042
Gabriel Ayala <gabeayala100@gmail.com>
2021-09-20 15:57:51 -0700

    Add lines to incorporate a total.

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -6,6 +6,7 @@ namespace Bakery
   {
     public static void Main()
     {
+      Console.Clear();
       Console.WriteLine("Welcome To Bakery de Pierre");
       Console.WriteLine("Bread is $5 each. Buy 2 Get 3rd free.");
       Console.WriteLine("Pastries are $2 each. Buy 2 Get 3rd for $1.");
@@ -15,38 +16,38 @@ namespace Bakery
 
       if (userChoice == "bread")
       {
-        Console.WriteLine("How many loaves of bread?");
+        Console.WriteLine("How many loaves of bread would you like?");
         string breadAmount = Console.ReadLine();
         int parsBreadAmount = int.Parse(breadAmount);
-        if (parsBreadAmount < 2)
-        {
-          Bread newBread = new Bread(parsBreadAmount);
+        Bread newBread = new Bread(parsBreadAmount);
         int breadPrice = newBread.SetPrice();
-        Console.WriteLine("For " + parsBreadAmount + " loaf your total comes to $" + breadPrice + ".");
-        } else 
+        Console.WriteLine("Will that complete your order or would you like to purchase another good?(type complete/another)");
+        string orderConfirm = Console.ReadLine().ToLower();
+        if (orderConfirm == "complete")
         {
-          Bread newBread = new Bread(parsBreadAmount);
-        int breadPrice = newBread.SetPrice();
-        Console.WriteLine("For " + parsBreadAmount + " loaves your total comes to $" + breadPrice + ".");
+          Console.WriteLine("For " + parsBreadAmount + ", your total comes to $" + breadPrice + ".");
+          Console.WriteLine("Thank you for your business <3");
+        } else if (orderConfirm == "another")
+        {
+          Console.WriteLine("How many pastries?");
+          string pastryAmount = Console.ReadLine();
+          int parsPastryAmount = int.Parse(pastryAmount);
+          Pastry newPastry = new Pastry(parsPastryAmount);
+          int pastryPrice = newPastry.SetPrice();
+          int totalPrice = OrderTotal.CalculatedTotal(pastryPrice, breadPrice);
+          Console.WriteLine("Amount of Pastries = " + parsPastryAmount + ", cost = $" + pastryPrice + ".");
+          Console.WriteLine("Amount of Bread = " + parsBreadAmount + ", cost = $" + breadPrice + ".");
+          Console.WriteLine("Your total comes to $" + totalPrice + ".");
+          Console.WriteLine("Thank you for your business <3");
         }
       } else if (userChoice == "pastry" || userChoice == "pastries")
       {
-        Console.WriteLine("How many pastries?");
+        Console.WriteLine("How many pastries would you like?");
         string pastryAmount = Console.ReadLine();
         int parsPastryAmount = int.Parse(pastryAmount);
-        if (parsPastryAmount < 2)
-        {
-          Pastry newPastry = new Pastry(parsPastryAmount);
-          int pastryPrice = newPastry.SetPrice();
-          Console.WriteLine("For " + parsPastryAmount + " pastry your total comes to $" + pastryPrice + ".");
-        } else 
-        {
-          Pastry newPastry = new Pastry(parsPastryAmount);
-          int pastryPrice = newPastry.SetPrice();
-          Console.WriteLine("For " + parsPastryAmount + " pastries your total comes to $" + pastryPrice + ".");
-        }
-        
-
+        Pastry newPastry = new Pastry(parsPastryAmount);
+        int pastryPrice = newPastry.SetPrice();
+        Console.WriteLine("For " + parsPastryAmount + ", your total comes to $" + pastryPrice + ".");
       }
     }
   }
