5b43a26b2de4df95b244f7d8a7c09b2772985964
Gabriel Ayala <gabeayala100@gmail.com>
2021-09-20 12:44:32 -0700

    Add conditionals for pastry selection and amount.

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -10,20 +10,43 @@ namespace Bakery
       Console.WriteLine("Bread is $5 each. Buy 2 Get 3rd free.");
       Console.WriteLine("Pastries are $2 each. Buy 2 Get 3rd for $1.");
       Console.WriteLine("-------------------------------------------");
-      Console.WriteLine("Would you like to purchase: Bread or Pastries");
+      Console.WriteLine("Would you like to purchase: type Bread or Pastry");
       string userChoice = Console.ReadLine().ToLower();
 
       if (userChoice == "bread")
       {
-        Console.WriteLine("How many loafs of bread?");
+        Console.WriteLine("How many loaves of bread?");
         string breadAmount = Console.ReadLine();
-        int parsedBreadAmount = int.Parse(breadAmount);
-
-        
-        Bread newBread = new Bread(parsedBreadAmount);
+        int parsBreadAmount = int.Parse(breadAmount);
+        if (parsBreadAmount < 2)
+        {
+          Bread newBread = new Bread(parsBreadAmount);
         int breadPrice = newBread.SetPrice();
+        Console.WriteLine("For " + parsBreadAmount + " loaf your total comes to $" + breadPrice + ".");
+        } else 
+        {
+          Bread newBread = new Bread(parsBreadAmount);
+        int breadPrice = newBread.SetPrice();
+        Console.WriteLine("For " + parsBreadAmount + " loaves your total comes to $" + breadPrice + ".");
+        }
+      } else if (userChoice == "pastry" || userChoice == "pastries")
+      {
+        Console.WriteLine("How many pastries?");
+        string pastryAmount = Console.ReadLine();
+        int parsPastryAmount = int.Parse(pastryAmount);
+        if (parsPastryAmount < 2)
+        {
+          Pastry newPastry = new Pastry(parsPastryAmount);
+          int pastryPrice = newPastry.SetPrice();
+          Console.WriteLine("For " + parsPastryAmount + " pastry your total comes to $" + pastryPrice + ".");
+        } else 
+        {
+          Pastry newPastry = new Pastry(parsPastryAmount);
+          int pastryPrice = newPastry.SetPrice();
+          Console.WriteLine("For " + parsPastryAmount + " pastries your total comes to $" + pastryPrice + ".");
+        }
+        
 
-        Console.WriteLine("For " + parsedBreadAmount + " loaves your total comes to $" + breadPrice + ".");
       }
     }
   }
